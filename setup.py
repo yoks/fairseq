be3da75fed80a9730616237fb752e81cb87f1301
Ivan Anisimov <inyokwetrust@gmail.com>
2021-05-25 20:58:14 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -207,7 +207,7 @@ def do_setup(package_data):
             'numpy; python_version>="3.7"',
             "regex",
             "sacrebleu>=1.4.12",
-            "torch",
+            "torch==1.8.1+cu111",
             "tqdm",
         ],
         dependency_links=dependency_links,
